349bb0b368bbc53c72cee281b71f21f58a6ad63f
vinaypokar12 <31915052+vinaypokar12@users.noreply.github.com>
2017-09-13 13:33:39 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -149,6 +149,15 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void removeFromElement(Student student) {
 		// Add your implementation here
+		if(student == null)
+			throw new IllegalArgumentException();
+		else{
+			LinkedList<Student> llstudent = new LinkedList<Student>(Arrays.asList(this.students));
+			int firstIndex = llstudent.indexOf(student);
+			for(int i=firstIndex+1;i<llstudent.size();i++)
+				llstudent.remove(i);
+			this.students = llstudent.toArray(new Student[llstudent.size()]);
+		}
 	}
 
 	@Override
